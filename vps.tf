fd47b10fd749b9f94e8b4029a8f340d20aaae078
Mostafa Negim <mostafa.negim@snapp.cab>
2022-04-24 00:12:21 +0430

    Test: Terraform plan by adding a sg

--- vps.tf
+++ vps.tf
@@ -7,3 +7,29 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "available" {}
+
+resource "aws_security_group" "test" {
+  name = "Hamid-request"
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "ssh"
+    from_port        = 22
+    protocol         = "tcp"
+    security_groups  = null
+    self             = false
+    to_port          = 22
+    ipv6_cidr_blocks = null
+    prefix_list_ids  = null
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "web"
+      from_port        = 80
+      protocol         = "tcp"
+      security_groups  = null
+      self             = false
+      to_port          = 80
+      ipv6_cidr_blocks = null
+      prefix_list_ids  = null
+  }]
+}
