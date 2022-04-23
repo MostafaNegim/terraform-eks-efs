b8652c0068c2df1fb4332d11a5da66624a9ee9df
Mostafa Negim <mostafa.negim@snapp.cab>
2022-04-24 01:29:43 +0430

    Update: versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,13 @@
-# terraform {
-  
-# }
+terraform {
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.8.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+   }
+}
