b8ed29931ed6bfec8bfb8ad9d5b720b9b1f917bb
Mostafa Negim <mostafa.negim@snapp.cab>
2022-04-27 17:06:06 +0430

    Update: Make mountpoint dynamically

--- storage/efs.tf
+++ storage/efs.tf
@@ -9,16 +9,40 @@ resource "aws_efs_file_system" "efs" {
 }
 
 resource "aws_efs_mount_target" "efs_mt" {
-  count           = length(data.aws_availability_zones.available.names)
+  count           = length(data.aws_instance.ec2_subnets)
   file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = data.aws_subnets.sns.ids[count.index]
-#   security_groups = [aws_security_group.efs.id]
+  subnet_id       = data.aws_instance.ec2_subnets[count.index].subnet_id
+  security_groups = ["${aws_security_group.ingress_efs.id}"]
 }
 
-data "aws_availability_zones" "available" {}
-data "aws_subnets" "sns" {
+
+
+# data "aws_availability_zones" "available" {}
+
+data "aws_subnets" "vpc_subnets" {
+    # vpc_id = 
   filter {
     name   = "vpc-id"
-    values = ["vpc-0307d6b94597a6c2c"]
+    values = [data.aws_vpc.eks_vpc.id]
   }
 }
+
+data "aws_instances" "ec2s" {
+  filter {
+    name   = "tag:kubernetes.io/cluster/${data.aws_eks_cluster.cluster.name}"
+    values = ["owned"]
+  }
+}
+
+data "aws_instance" "ec2_subnets" {
+  # for_each    = toset(data.aws_instances.ec2s.ids)
+  count    = length(data.aws_instances.ec2s.ids)
+  instance_id = data.aws_instances.ec2s.ids[count.index]
+}
+
+# data "aws_ec2_instance_types" "test" {
+#   filter {
+#     name   = "tag:kubernetes.io/cluster/${data.aws_eks_cluster.cluster.name}"
+#     values = ["owned"]
+#   }
+# }
--- storage/outputs.tf
+++ storage/outputs.tf
@@ -1,6 +1,18 @@
-output "available" {
-  value = data.aws_availability_zones.available
-}
-output "subnets" {
-  value = data.aws_subnets.sns
+# output "ec2s" {
+#   value = data.aws_instances.ec2s
+# }
+# output "subnets" {
+#   value = data.aws_subnets.sns
+# }
+
+# output "vpc" {
+#   value = data.aws_vpc.eks_vpc
+# }
+
+# output "eks" {
+#   value = data.aws_eks_cluster.cluster
+# }
+
+output "ec2_subnets" {
+  value = data.aws_instance.ec2_subnets
 }
